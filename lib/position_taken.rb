9f25a9a45c48d6ee687cf1fb971d47b470af5de7
Arnold Su <arnoldtingsu@gmail.com>
2018-08-19 03:46:42 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,10 @@ board = Array.new(9, " ")
 
 def position_taken?(board, position)
 
+<<<<<<< HEAD
  board[position] == " " || board[position] == nil || board[position] == ""? false :true
 
  end
+=======
+ board[position] != " " || nil || ""?
+>>>>>>> ffb4df85af6c792f7d645d72d83ba89e6205b86a
